56102240ccfe6da86c733a0f83c0390eb84607ad
Shahrukh Ahmed Siddiqui <shahrukh.amd@gmail.com>
2018-05-16 19:18:43 +0530

    Implemented bottom navigation bar

--- app/src/main/java/com/sasiddiqui/pseudodata/presentation/ui/activity/MainActivity.java
+++ app/src/main/java/com/sasiddiqui/pseudodata/presentation/ui/activity/MainActivity.java
@@ -1,15 +1,70 @@
 package com.sasiddiqui.pseudodata.presentation.ui.activity;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.sasiddiqui.pseudodata.R;
+import com.sasiddiqui.pseudodata.presentation.ui.fragment.AlbumListFragment;
+import com.sasiddiqui.pseudodata.presentation.ui.fragment.PostListFragment;
+import com.sasiddiqui.pseudodata.presentation.ui.fragment.UserListFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.main_bottom_navigation_bar)
+    BottomNavigationView bottomNavigationView;
+
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.main_fragment_container, new PostListFragment())
+                .commit();
+
+        // TODO Replace all these hacks with proper MVP design.
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int selectedItem = bottomNavigationView.getSelectedItemId();
+
+                switch (item.getItemId()) {
+                    case R.id.bottom_nav_posts:
+                        if (selectedItem == R.id.bottom_nav_posts) return false;
+                        fragmentManager.beginTransaction()
+                                .replace(R.id.main_fragment_container, new PostListFragment())
+                                .commit();
+                        return true;
+
+                    case R.id.bottom_nav_albums:
+                        if (selectedItem == R.id.bottom_nav_albums) return false;
+                        fragmentManager.beginTransaction()
+                                .replace(R.id.main_fragment_container, new AlbumListFragment())
+                                .commit();
+                        return true;
+
+                    case R.id.bottom_nav_users:
+                        if (selectedItem == R.id.bottom_nav_users) return false;
+                        fragmentManager.beginTransaction()
+                                .replace(R.id.main_fragment_container, new UserListFragment())
+                                .commit();
+                        return true;
+                }
+
+                return false;
+            }
+        });
     }
+
 }
